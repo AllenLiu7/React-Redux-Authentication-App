86e556c557a118c6b7d40a9056058631dda87c06
Allen Liu <allen6qi@gmail.com>
2020-06-12 22:40:44 +0800

    finished login and signup route with passport local strategy

---
+++ models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+//const Schema = mongoose.Schema;
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  googleId: String, //this is the ID that google return
+  secret: String,
+});
+
+//hash the password before store
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  //Hashes the password sent by the user for login and checks if the hashed password stored in the
+  //database matches the one sent. Returns true if it does else false.
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+module.exports = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-var compression = require('compression');
+const compression = require('compression');
+const passport = require('passport');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+//if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,19 +14,40 @@ const port = process.env.PORT || 5000;
 //connect to DB
 connectDB();
 
+require('./auth/auth');
+
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+//   app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
+//Define Routes
+app.get('/', function (req, res) {
+  res.send('connected');
+});
+app.use('/login', require('./routes/api/login'));
+app.use('/signup', require('./routes/api/signup'));
+//We plugin our jwt strategy as a middleware so only verified users can access this route
+// app.use(
+//   '/secrets',
+//   passport.authenticate('jwt', { session: false }),
+//   require('./routes/api/secrets')
+// );
+
+//Handle Errors
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
 
 app.listen(port, (error) => {
   if (error) throw error;
