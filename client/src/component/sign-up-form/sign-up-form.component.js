24ac3b20b958571f0deb1adccd5c227b16e25b8b
Allen Liu <allen6qi@gmail.com>
2020-06-13 20:14:19 +0800

    connected signup form to api and successfully register new user

--- client/src/component/sign-up-form/sign-up-form.component.js
+++ client/src/component/sign-up-form/sign-up-form.component.js
@@ -1,37 +1,72 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './sign-up-form.styles.scss';
 
 export default class SignUpform extends Component {
   state = {
+    email: '',
+    password: '',
+    confirmPassword: '',
     validated: false,
   };
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password, confirmPassword } = this.state;
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    this.setState({ validated: true });
+
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
+    axios
+      .post('http://localhost:5000/signup', {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
-    const handleSubmit = (event) => {
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-
-      this.setState({ validated: true });
-    };
-
     return (
       <div className='sign-up-form'>
         <Form
           noValidate
           validated={this.state.validated}
-          onSubmit={handleSubmit}
+          onSubmit={this.handleSubmit}
         >
           <Form.Group controlId='formBasicEmail'>
             <Form.Label>Email address</Form.Label>
-            <Form.Control required type='email' placeholder='Enter email' />
-            <Form.Text className='text-muted'>
-              We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Control
+              required
+              type='email'
+              name='email'
+              placeholder='Enter email'
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
             <Form.Control.Feedback type='invalid'>
               Please enter a username.
             </Form.Control.Feedback>
@@ -39,22 +74,36 @@ export default class SignUpform extends Component {
 
           <Form.Group controlId='formBasicPassword'>
             <Form.Label>Password</Form.Label>
-            <Form.Control required type='password' placeholder='Password' />
+            <Form.Control
+              required
+              type='password'
+              name='password'
+              placeholder='Password'
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
             <Form.Control.Feedback type='invalid'>
-              Please enter a password.
+              Please enter password.
             </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId='formBasicPassword'>
             <Form.Label>Comfirm Password</Form.Label>
-            <Form.Control required type='password' placeholder='Password' />
+            <Form.Control
+              required
+              type='password'
+              name='confirmPassword'
+              placeholder='Password confirmation'
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+            />
             <Form.Control.Feedback type='invalid'>
-              Please enter a password again.
+              Please enter password again.
             </Form.Control.Feedback>
           </Form.Group>
 
           <Button variant='primary' type='submit'>
-            Submit
+            Register
           </Button>
         </Form>
       </div>
